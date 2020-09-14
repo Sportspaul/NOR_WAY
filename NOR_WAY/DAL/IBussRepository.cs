d7934b2c8a1629ea75f24f76bfa07e99ed9d966a
danielrogerh <daniel.brann@hotmail.no>
2020-09-14 12:07:41 +0200

    - Interface for metodene som snakker med databasen

---
+++ NOR_WAY/DAL/IBussRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NOR_WAY.Model;
+
+namespace NOR_WAY.DAL
+{
+    public interface IBussRepository
+    {
+        Task<List<Stopp>> HentAlleStopp();
+        Task<Billettyper> HentAlleBillettyper();
+        Task<List<Avgang>> FinnAktuelleRuter(AvgangParam param);
+        Task<bool> FullforOrdre(KundeOrdre ordre);
+    }
+}

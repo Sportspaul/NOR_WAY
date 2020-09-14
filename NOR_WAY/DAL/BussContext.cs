5df8ba961e4abfde766141f80708e3475870ffb3
danielrogerh <daniel.brann@hotmail.no>
2020-09-14 11:08:13 +0200

    - Definerte klasser som skal brukes til å opprette databasen
    - Skrev kode for oppretting av databasetabellene

---
+++ NOR_WAY/DAL/BussContext.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace NOR_WAY.DAL
+{
+    public class Ruter
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public string Linjekode { get; set; } // PK
+        public string Rutenavn { get; set; }
+        public int Startpris { get; set; }
+        public int TilleggPerStopp { get; set; }
+        public int Kapasitet { get; set; }
+    }
+
+    public class Stopp
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public string Navn { get; set; } // PK
+    }
+
+    public class RuteStopp
+    {
+        public int Id { get; set; } // PK
+        public int StoppNummer { get; set; }
+        public int MinutterTilNesteStopp { get; set; }
+
+        // Foreign Keys
+        virtual public Stopp Navn { get; set; } // FK
+        virtual public Ruter Linjekode { get; set; } // FK
+    }
+
+    public class Avganger
+    {
+        public int Id { get; set; } // PK
+        public string Dato { get; set; }
+        public string Avreise { get; set; }
+        public string Ankomst { get; set; }
+        public int SolgteBilletter { get; set; }
+
+        // Foreign Key
+        virtual public Ruter Linjekode { get; set; } // FK
+    }
+
+    public class Ordre
+    {
+        public int Id { get; set; } // PK
+        public string Epost { get; set; }
+        public string StartStopp { get; set; }
+        public string SluttStopp { get; set; }
+        public int Sum { get; set; }
+
+        // Foreign Keys
+        virtual public Ruter Linjekode { get; set; } // FK
+        virtual public Avganger Avganger { get; set; } // FK
+        virtual public Billettyper Billettype { get; set; } // FK
+    }
+
+    public class Billettyper
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public string Billettype { get; set; } // PK
+        public int Rabattsats { get; set; }
+    }
+
+    public class BussContext : DbContext
+    {
+        public BussContext(DbContextOptions<BussContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Ruter> Ruter { get; set; }
+        public DbSet<Stopp> Stopp { get; set; }
+        public DbSet<RuteStopp> RuteStopp { get; set; }
+        public DbSet<Avganger> Avganger { get; set; }
+        public DbSet<Ordre> Ordre { get; set; }
+        public DbSet<Billettyper> Billettyper { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // må importere pakken Microsoft.EntityFrameworkCore.Proxies
+            // og legge til"viritual" på de attriuttene som ønskes å lastes automatisk (LazyLoading)
+            optionsBuilder.UseLazyLoadingProxies();
+        }
+    }
+}

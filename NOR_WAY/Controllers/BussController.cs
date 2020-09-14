3c70e867103b3eeb76019042c64249abe0f445ee
danielrogerh <daniel.brann@hotmail.no>
2020-09-14 12:34:42 +0200

    * BussController.cs: - Oprettet Controller-klasse
    * BussRepository.cs: - Opprettet kopi av IBussRepository - Ingen
      metoder implementert ennå

---
+++ NOR_WAY/Controllers/BussController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NOR_WAY.DAL;
+using NOR_WAY.Model;
+
+namespace NOR_WAY.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class BussController : ControllerBase
+    {
+        private readonly IBussRepository _db;
+        public BussController(IBussRepository db)
+        {
+            _db = db;
+        }
+
+        public async Task<List<Stopp>> HentAlleStopp()
+        {
+            return await _db.HentAlleStopp();
+        }
+
+        public async Task<Billettyper> HentAlleBillettyper()
+        {
+            return await _db.HentAlleBillettyper();
+        }
+
+        public async Task<List<Avgang>> FinnAktuelleAvganger(AvgangParam param)
+        {
+            return await _db.FinnAktuelleAvganger(param);
+        }
+
+        public async Task<bool> FullforOrdre(KundeOrdre ordre)
+        {
+            return await _db.FullforOrdre(ordre);
+        }
+    }
+}
---
+++ NOR_WAY/DAL/BussRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NOR_WAY.Model;
+
+namespace NOR_WAY.DAL
+{
+    public class BussRepository : IBussRepository
+    {
+        private readonly BussContext _db;
+
+        public BussRepository(BussContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<List<Avgang>> FinnAktuelleAvganger(AvgangParam param)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> FullforOrdre(KundeOrdre ordre)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Billettyper> HentAlleBillettyper()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Stopp>> HentAlleStopp()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

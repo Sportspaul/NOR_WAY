7e7d446fde01d7da3c3d9028d4e46a6f7dd03552
danielrogerh <daniel.brann@hotmail.no>
2020-09-15 17:04:53 +0200

    - Satt opp New Solution: NOR_WAY_Tests som skal brukes til enhetstesting

---
+++ NOR_WAY_Tests/BussTests.cs
@@ -0,0 +1,15 @@
+using System.Threading.Tasks;
+using Moq;
+using Xunit;
+
+namespace NOR_WAY_Tests
+{
+    public class BussTests
+    {
+        [Fact]
+        public async Task FinnNesteAvgang()
+        {
+
+        }
+    }
+}

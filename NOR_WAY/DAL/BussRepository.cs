9b766eb7a8ea9d0095cc441d62fcbf8065b6b9a4
danielrogerh <daniel.brann@hotmail.no>
2020-09-16 16:54:18 +0200

    * BussRepository.cs: - Førsteutkast av FinnNesteAvgang-metoden
    * BussTests.cs: - Forsøk på testing av FinnNesteAvgang-metoden

--- NOR_WAY/DAL/BussRepository.cs
+++ NOR_WAY/DAL/BussRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
@@ -18,7 +19,111 @@ namespace NOR_WAY.DAL
 
         public async Task<Avgang> FinnNesteAvgang(AvgangParam param)
         {
-            throw new NotImplementedException();
+
+            Stopp startStopp = _db.Stopp.FirstOrDefault(s => s.Navn == param.StartStopp);
+            Stopp sluttStopp = _db.Stopp.FirstOrDefault(s => s.Navn == param.StartStopp);
+
+            // Finner alle Rutene som inkluderer påstigning og som inkluderer avstigning
+            List<Ruter> startStoppRuter = FinnRuter(startStopp);
+            List<Ruter> sluttStoppRuter = FinnRuter(sluttStopp);
+
+            // Finner alle rutene påstigning og avstigning har til felles
+            Ruter fellesRute = FinnFellesRute(startStoppRuter, sluttStoppRuter);
+
+            // Finne ut hvilket stoppNummer påstigning og avstigning har i den felles ruten
+            int stoppNummer1 = FinnStoppNummer(startStopp, fellesRute);
+            int stoppNummer2 = FinnStoppNummer(sluttStopp, fellesRute);
+
+            // Beregner reisetiden fra stopp 1 til stopp 2
+            int reisetid = BeregnReisetid(stoppNummer1, stoppNummer2);
+
+            // Finne neste avgang ved å søke i databasen to ulike måter basert på brukerinput: 
+            // Finne ut hvordan å håndtere dato og tidspunkter mtp sammenligning av verdier fra brukerinput og i databasen
+            string innAvreise = param.Dato + " " + param.Tidspunkt;
+            DateTime avreise = DateTime.ParseExact(innAvreise, "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture);
+
+            Avganger nesteAvgang = new Avganger();
+            if (param.AvreiseEtter == true) // Hvis Avreise Etter:
+            {
+                nesteAvgang = _db.Avganger
+                    .Where(a => a.Rute == fellesRute)
+                    .SingleOrDefault(a => a.Avreise >= avreise);
+            }
+            else  // Hvis Ankomst Før:
+            {
+                DateTime ankomst = avreise.AddMinutes(-reisetid);
+                nesteAvgang = _db.Avganger
+                    .Where(a => a.Rute == fellesRute)
+                    .SingleOrDefault(a => a.Avreise <= ankomst);
+            }
+
+            // Opretter Avgang-objektet som skal sendes til klienten
+            string utAvreise = nesteAvgang.Avreise.ToString("yyyy-MM-dd HH:mm");
+            string utAnkomst = nesteAvgang.Avreise.AddMinutes(reisetid).ToString("yyyy-MM-dd HH:mm");
+            Avgang utAvgang = new Avgang
+            {
+                AvgangId = nesteAvgang.Id,
+                Rutenavn = fellesRute.Rutenavn,
+                Linjekode = fellesRute.Linjekode,
+                // TODO: Beregn pris, basert på startpris og antall stopp
+                Pris = 100,
+                Avreise = utAvreise,
+                Ankomst = utAnkomst,
+                Reisetid = reisetid
+            };
+
+            Console.Write("Utskrift: " + utAvgang.AvgangId);
+            return utAvgang;
+        }
+
+        /* Hjelpemetode som tar inn et stopp object og returnerer en
+        liste med ruter som innholder stoppet */
+        private List<Ruter> FinnRuter(Stopp stopp)
+        {
+            return _db.RuteStopp.Where(rs => rs.Stopp == stopp).Select(rs => rs.Rute).ToList();
+        }
+
+        // Hjelpemetode som finner Rutern to lister med Ruter har til felles
+        private Ruter FinnFellesRute(List<Ruter> startStoppRuter, List<Ruter> sluttStoppRuter)
+        {
+            Ruter fellesRute = new Ruter();
+            foreach (Ruter startStoppRute in startStoppRuter)
+            {
+                foreach (Ruter sluttStoppRute in sluttStoppRuter)
+                {
+                    if (startStoppRute == sluttStoppRute)
+                    {
+                        fellesRute = startStoppRute;
+                    }
+                }
+            }
+
+            return fellesRute;
+        }
+
+        // Hjelpemetode som finner stoppnummeret til et spesifikt stopp i en spesifikk rute
+        private int FinnStoppNummer(Stopp startStopp, Ruter fellesRute)
+        {
+            RuteStopp ruteStopp = _db.RuteStopp.FirstOrDefault(
+                        rs => rs.Stopp == startStopp && rs.Rute == fellesRute);
+
+            return ruteStopp.StoppNummer;
+        }
+
+        // Hjelpemetode som beregner reisetiden fra startStopp til sluttStopp
+        private int BeregnReisetid(int startStopp, int sluttStopp)
+        {
+            // Beregner reisetid
+            List<int> minTilNesteStoppList = _db.RuteStopp
+                .Where(rs => rs.StoppNummer > startStopp && rs.StoppNummer < sluttStopp)
+                .Select(rs => rs.MinutterTilNesteStopp).ToList();
+
+            int reisetid = 0;
+            foreach (int minutter in minTilNesteStoppList)
+            {
+                reisetid += minutter;
+            }
+            return reisetid;
         }
 
         
--- NOR_WAY_Tests/BussTests.cs
+++ NOR_WAY_Tests/BussTests.cs
@@ -1,15 +1,56 @@
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Moq;
+using NOR_WAY.Controllers;
+using NOR_WAY.DAL;
+using NOR_WAY.Model;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace NOR_WAY_Tests
 {
     public class BussTests
     {
         [Fact]
-        public async Task FinnNesteAvgang()
+        public async Task FinnNesteAvgangTest()
         {
+            var param = new AvgangParam
+            {
+                StartStopp = "Bergen",
+                SluttStopp = "Vadheim",
+                Dato = "2020-11-20",
+                Tidspunkt = "16:00",
+                AvreiseEtter = true
+            };
 
+            var forventetAvgang = new Avgang
+            {
+                AvgangId = 1,
+                Rutenavn = "Fjordekspressen",
+                Linjekode = "NW431",
+                Pris = 100,
+                Avreise = "2020-11-25 17:00",
+                Ankomst = "2020-11-25 18:20",
+                Reisetid = 80
+            };
+
+            var mock = new Mock<IBussRepository>();
+            mock.Setup(b => b.FinnNesteAvgang(param)).ReturnsAsync(forventetAvgang);
+            var bussController = new BussController(mock.Object);
+            Avgang resultat = await bussController.FinnNesteAvgang(param);
+
+            Console.WriteLine("AvgangId " + resultat.AvgangId);
+            Console.WriteLine("Rutenavn " + resultat.Rutenavn);
+            Console.WriteLine("Reisetid " + resultat.Reisetid);
+
+            Assert.Equal(forventetAvgang.AvgangId, resultat.AvgangId);
+            Assert.Equal(forventetAvgang.Rutenavn, resultat.Rutenavn);
+            Assert.Equal(forventetAvgang.Linjekode, resultat.Linjekode);
+            Assert.Equal(forventetAvgang.Pris, resultat.Pris);
+            Assert.Equal(forventetAvgang.Avreise, resultat.Avreise);
+            Assert.Equal(forventetAvgang.Ankomst, resultat.Ankomst);
+            Assert.Equal(forventetAvgang.Reisetid, resultat.Reisetid);
         }
     }
 }

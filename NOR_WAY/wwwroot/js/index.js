3fb50ca96237dd77f5ad5530feb58854474288a5
danielrogerh <daniel.brann@hotmail.no>
2020-09-18 20:47:21 +0200

    - Brukeren får forslag til stopp som matcher starten av inputen deres

--- NOR_WAY/wwwroot/js/index.js
+++ NOR_WAY/wwwroot/js/index.js
@@ -4,12 +4,12 @@
     leggTilDato();
 });
 
-let billettyper;
 
 function hentAlleStopp() {
     $.get("Buss/HentAlleStopp", function (alleStopp) {
-        console.table(alleStopp)
-        printStopp(alleStopp);
+        console.log(alleStopp[0]);
+        stoppforslag($("#startStopp"), $("#auto1"), alleStopp);
+        stoppforslag($("#sluttStopp"), $("#auto2"), alleStopp);
     });
 }
 
@@ -20,7 +20,7 @@ function hentAlleBillettyper() {
         var $dropdown = $("#billettype1");
         billettyper = alleBillettyper;
         $.each(alleBillettyper, function () {
-            $dropdown.append($("<option />").val(this.billettype).text(this.billettype + ", -" + this.rabattsats + "%"));
+            $dropdown.append($("<option />").val(this.billettype).text(this.billettype));
         });
     });
 }
@@ -70,7 +70,7 @@ function leggTilBillett() {
     console.log(billettyper);
     var $dropdown = $(`#${id}`);
     $.each(billettyper, function () {
-        $dropdown.append($("<option />").val(this.billettype).text(this.billettype + ", -" + this.rabattsats + "%"));
+        $dropdown.append($("<option />").val(this.billettype).text(this.billettype));
     });
 }
 
@@ -85,7 +85,7 @@ function leggTilDato() {
 // Kun for testing
 
 function printStopp(alleStopp) {
-    let stoppListe = new Array();
+    let stoppListe = new stoppListeay();
 
     for (stopp of alleStopp) {
         stoppListe.push(stopp.navn.toLowerCase());
@@ -109,4 +109,116 @@ function titleCase(str) {
         splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
     }
     return splitStr.join(' ');
+}
+
+// function for å gi brukeren live-stoppforslag,
+function stoppforslag(inputfelt, utskrift, stoppArray) {
+    var fokusert;
+
+    // Eventlisner på intput feltet
+    inputfelt.on("input", function (e) {
+        let stoppListe, stoppElement, i
+        let val = this.value;
+
+        // Lukker eksiterende liste
+        lukkAlleLister();
+        if (!val) { return false; }
+        fokusert = -1;
+
+        // Lager en div som vil inneholde stoppene
+        stoppListe = document.createElement("div");
+        stoppListe.setAttribute("id", this.id + "stoppListe");
+        stoppListe.setAttribute("class", "stoppListe");
+        this.append(stoppListe);
+
+        for (i = 0; i < stoppArray.length; i++) {
+
+            // Sjekker om stopp i listen starter med de samme bokstavene som input
+            if (stoppArray[i].navn.substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+
+                // Opretter en div for hvert stopp som marcher
+                stoppElement = document.createElement("DIV");
+
+                // Gjør bokstavene som matcher bold
+                stoppElement.innerHTML = "<strong>" + stoppArray[i].navn.substr(0, val.length) + "</strong>";
+                stoppElement.innerHTML += stoppArray[i].navn.substr(val.length);
+
+                // Tar vare på verdien til stoppene som matcher i et input feltet
+                stoppElement.innerHTML += "<input type='hidden' value='" + stoppArray[i].navn + "'>";
+
+                // Event listner for om noen trykker på et av de foreslåtte stoppene
+                stoppElement.addEventListener("click", function (e) {
+
+                    // Fyller input feltet med stoppet som brukeren trykker på
+                    inputfelt.val(this.getElementsByTagName("input")[0].value);
+
+                    // Lukker listen med forslag til stopp
+                    lukkAlleLister();
+                });
+
+                // Legger stopp elementet til stoppListe elementet og skriver listen ut til brukeren
+                stoppListe.appendChild(stoppElement);
+                utskrift.append(stoppListe);
+            }
+        }
+    });
+
+    inputfelt.on("keydown", function (e) {
+        var emnt = document.getElementById(this.id + "stoppListe");
+        if (elmt) elmt = elmt.getElementsByTagName("div");
+
+        // Hvis brukeren trykker piltast ned
+        if (e.keyCode == 40) {
+
+            // Flytter pekeren til aktivt element en videre i listen
+            fokusert++;
+
+            // Endrer style på aktivt element
+            leggTilAktiv(elmt);
+
+        // Hvis brukeren trykker piltast opp
+        } else if (e.keyCode == 38) { 
+
+            // Flytter pekeren til aktivt element en tilbake i listen
+            fokusert--;
+
+            // Endrer style på aktivt element
+            leggTilAktiv(elmt);
+        } 
+    });
+
+    // Setter et element til å være aktivt
+    function leggTilAktiv(elmt) {
+        if (!elmt) return false;
+
+        // Fjerner style for aktivt element fra alle elementene
+        fjernAktiv(elmt);
+        if (fokusert >= elmt.length) fokusert = 0;
+        if (fokusert < 0) fokusert = (elmt.length - 1);
+
+        // Legger til style-klasse for aktivt element
+        elmt[fokusert].classList.add("forslag-active");
+    }
+
+    // Fjerner style-klassen for aktivt element fra alle elementer
+    function fjernAktiv(elmt) {
+        for (var i = 0; i < elmt.length; i++) {
+            elmt[i].classList.remove("forslag-active");
+        }
+    }
+
+    // Lukker listen med forslag til stopp
+    function lukkAlleLister(elmnt) {
+        var elmt = document.getElementsByClassName("stoppListe");
+        for (var i = 0; i < elmt.length; i++) {
+            if (elmnt != x[i] && elmnt != inputfelt) {
+                elmt[i].parentNode.removeChild(x[i]);
+            }
+        }
+    }
+
+    // Lukker listen med stopp hvis bruker trykker utenfor listen
+    $(document).on("click", function (e) {
+        lukkAlleLister(e.target);
+    });
 }

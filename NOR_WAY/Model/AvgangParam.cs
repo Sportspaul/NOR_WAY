1e64df4f06a5a3a3446defaf1a719da37226e0c6
Paul Høglend <pmhoglend@gmail.com>
2020-09-18 16:19:28 +0200

    -Lagt til regEx på stoppene i avgangsparametere

--- NOR_WAY/Model/AvgangParam.cs
+++ NOR_WAY/Model/AvgangParam.cs
@@ -1,13 +1,17 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace NOR_WAY.Model
 {
     // TODO: Se på mulighet for navnendring
     public class AvgangParam
     {
+        [RegularExpression(@"[a-zA-ZæøåÆØÅ. \-]{2,20}")]
         public string StartStopp { get; set; }
+        [RegularExpression(@"[a-zA-ZæøåÆØÅ. \-]{2,20}")]
         public string SluttStopp { get; set; }
         public string Dato { get; set; }
-        public string Tidspunkt { get; set; }
+        public string Tidspunkt { get; set; } 
         public bool AvreiseEtter { get; set; } // TODO: Se på mulighet for navnendring
     }
 }

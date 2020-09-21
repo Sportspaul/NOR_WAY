30edaacefe4e4d5347329769dd21e2d97e035bd2
Paul Høglend <pmhoglend@gmail.com>
2020-09-21 17:30:44 +0200

    Logger feil -finnNesteAvgang

--- NOR_WAY/Controllers/BussController.cs
+++ NOR_WAY/Controllers/BussController.cs
@@ -39,7 +39,9 @@ namespace NOR_WAY.Controllers
             if(ModelState.IsValid)
             {
                 Avgang nesteAvgang = await _db.FinnNesteAvgang(input);
-                if (nesteAvgang == null) { return BadRequest("Feil i inputvalideringen på server"); }
+                if (nesteAvgang == null) {
+                    _log.LogInformation("Feil i inputvalideringen på server");
+                    return BadRequest("Feil i inputvalideringen på server"); }
                 return Ok(nesteAvgang);
             }
                 

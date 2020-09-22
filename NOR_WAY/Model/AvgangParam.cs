7ea07880096d2903f1964a33fc131ffd760f8498
danielrogerh <daniel.brann@hotmail.no>
2020-09-22 21:56:24 +0200

    Models mappen - La til manglende regexer og kommenterte hva modellenes hensikt er

--- NOR_WAY/Model/AvgangParam.cs
+++ NOR_WAY/Model/AvgangParam.cs
@@ -4,17 +4,23 @@ using System.ComponentModel.DataAnnotations;
 
 namespace NOR_WAY.Model
 {
+    // Modell for å ta imot nødvendige verdier fra klienten for å beregne neste mulige avgang
     public class AvgangParam
     {
         [RegularExpression(@"[a-zA-ZæøåÆØÅ. \-]{2,20}")]
         public string StartStopp { get; set; }
+
         [RegularExpression(@"[a-zA-ZæøåÆØÅ. \-]{2,20}")]
         public string SluttStopp { get; set; }
+
         [RegularExpression(@"([0-9]{4})[-]([0-9]{2})[-]([0-9]{2})")]
         public string Dato { get; set; }
+
         [RegularExpression(@"([0-9]{2})[:]([0-9]{2})")]
         public string Tidspunkt { get; set; }
+
         public bool AvreiseEtter { get; set; }
+
         public List<string> Billettyper { get; set; }
     }
 }

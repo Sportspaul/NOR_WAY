1511db4f05bbae2a5cd32ca1453381194888153b
Paul Høglend <pmhoglend@gmail.com>
2020-09-23 14:01:15 +0200

    Modelklasse RuteData

---
+++ NOR_WAY/Model/RuteData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NOR_WAY.DAL;
+
+namespace NOR_WAY.Model
+{
+    public class RuteData
+    {
+        public string StoppNavn { get; set; }
+        public string Linjekode { get; set; }
+        public string Rutenavn { get; set; }
+        public List<Avganger> RuteAvganger { get; set; }
+    }
+}

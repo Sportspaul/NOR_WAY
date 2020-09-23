a0a0b880a821251b6b5e2aa45562b75d0c6ff821
Paul Høglend <pmhoglend@gmail.com>
2020-09-23 12:15:38 +0200

    Feilretting

--- NOR_WAY/wwwroot/js/index.js
+++ NOR_WAY/wwwroot/js/index.js
@@ -133,7 +133,7 @@ function finnNesteAvgang() {
         Dato: dato,
         Tidspunkt: tidspunkt,
         AvreiseEtter: avreiseEtter,
-        Billettyper: billettyper 
+        Billettyper: billettyper
     }
 
     if (validerAvgangInput()) {
@@ -234,179 +234,179 @@ function finnNesteAvgang() {
             });
         }).fail(function () {
 
-        // Gir brukeren tilbakemelding hvis ingen avganger ble hentet 
-        $("#avgang").css("display", "none");
-        $("#avgang").html("");
-        $("#feilAvgang").html("Vi tilbyr deverre ikke reisen du ønsker");
-    });
-}
-
-
-
-function finnReisetid(reiseTid) {
-    let min = reiseTid % 60;
-    let time = Math.floor(reiseTid / 60);
-    let utReisetid = "Reisetid: " + time;
-
-    if (time != 1) {
-        utReisetid += " timer";
-        if (min != 1) {
-            utReisetid += min + " minutter";
-            
-        } else {
-            utReisetid += min + " minutt";
-            
-        }
-    } else {
-        utReisetid += " time";
-        if (min != 1) {
-            utReisetid += min + " minutter";
-            
-        } else {
-            utReisetid += min + " minutt";
-            
-        }
+            // Gir brukeren tilbakemelding hvis ingen avganger ble hentet 
+            $("#avgang").css("display", "none");
+            $("#avgang").html("");
+            $("#feilAvgang").html("Vi tilbyr deverre ikke reisen du ønsker");
+        });
     }
-    return utReisetid;
-}
 
-// Henter ut data ut fra strenger
-function hentTidsdata(tidInput) {
-    let datoVerdier = tidInput.substr(0, 10).split("-")
-    let tidVerdier = tidInput.substr(11, 5).split(":").map(Number)
-    let tidspunkt = [datoVerdier, tidVerdier] //todimensjonalt array
-    return tidspunkt
-}
 
-.
-// Legger til en ny select og fyller den med billettyer-data
-function leggTilBillett() {
-    const antall = $('.billettype').length;
-    const id = `billettype${antall+1}`;
 
-    $('#billetter').append(`<select id="${id}" class="form-control billettype mb-2"></select>`);
-    console.log(billettyper);
-    var $dropdown = $(`#${id}`);
-    $.each(billettyper, function () {
-        $dropdown.append($("<option />").val(this.billettype).text(this.billettype));
-    });
-}
+    function finnReisetid(reiseTid) {
+        let min = reiseTid % 60;
+        let time = Math.floor(reiseTid / 60);
+        let utReisetid = "Reisetid: " + time;
 
-// Gir brukeren live-stoppforslag
-function stoppforslag(inputfelt, utskrift, stoppArray, feilmelding) {
-    var fokusert;
+        if (time != 1) {
+            utReisetid += " timer";
+            if (min != 1) {
+                utReisetid += min + " minutter";
 
-    // Eventlisner på intput feltet 
-    inputfelt.on("input", function (e) {
-        let stoppListe, stoppElement, i
-        let val = this.value;
+            } else {
+                utReisetid += min + " minutt";
 
-        // Lukker eksiterende liste
-        lukkAlleLister();
-        if (!val) { return false; }
-        fokusert = -1;
+            }
+        } else {
+            utReisetid += " time";
+            if (min != 1) {
+                utReisetid += min + " minutter";
 
-        // Lager en div som vil inneholde stoppene
-        stoppListe = document.createElement("div");
-        stoppListe.setAttribute("id", this.id + "stoppListe");
-        stoppListe.setAttribute("class", "stoppListe");
-        this.append(stoppListe);
+            } else {
+                utReisetid += min + " minutt";
 
-        // Looper gjennom alle stoppene i listen med stopp
-        for (i = 0; i < stoppArray.length; i++) {
-
-            // Sjekker om stopp i listen starter med de samme bokstavene som input
-            if (stoppArray[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-                feilmelding.html("");
-
-                // Opretter en div for hvert stopp som marcher
-                stoppElement = document.createElement("DIV");
-
-                // Gjør bokstavene som matcher bold
-                stoppElement.innerHTML = "<strong>" + stoppArray[i].substr(0, val.length) + "</strong>";
-                stoppElement.innerHTML += stoppArray[i].substr(val.length);
-
-                // Tar vare på verdien til stoppene som matcher i et input feltet
-                stoppElement.innerHTML += "<input type='hidden' value='" + stoppArray[i] + "'>";
-
-                // Event listner for om noen trykker på et av de foreslåtte stoppene
-                stoppElement.addEventListener("click", function (e) {
-
-                    // Fyller input feltet med stoppet som brukeren trykker på 
-                    inputfelt.val(this.getElementsByTagName("input")[0].value);
-
-                    // Lukker listen med forslag til stopp
-                    lukkAlleLister();
-                });
-
-                // Legger stopp elementet til stoppListe elementet og skriver listen ut til brukeren
-                stoppListe.appendChild(stoppElement);
-                utskrift.append(stoppListe);
             }
         }
-    });
+        return utReisetid;
+    }
 
-    // EventListener på om en tast blir trykket
-    inputfelt.on("keydown", function (e) {
-        var elmt = document.getElementById(this.id + "stoppListe");
-        if (elmt) elmt = elmt.getElementsByTagName("div");
+    // Henter ut data ut fra strenger
+    function hentTidsdata(tidInput) {
+        let datoVerdier = tidInput.substr(0, 10).split("-")
+        let tidVerdier = tidInput.substr(11, 5).split(":").map(Number)
+        let tidspunkt = [datoVerdier, tidVerdier] //todimensjonalt array
+        return tidspunkt
+    }
 
-        // Hvis brukeren trykker piltast ned
-        if (e.keyCode == 40) {
+    // Legger til en ny select og fyller den med billettyer-data
+    function leggTilBillett() {
+        const antall = $('.billettype').length;
+        const id = `billettype${antall + 1}`;
 
-            // Flytter pekeren til aktivt element en videre i listen
-            fokusert++;
+        $('#billetter').append(`<select id="${id}" class="form-control billettype mb-2"></select>`);
+        console.log(billettyper);
+        var $dropdown = $(`#${id}`);
+        $.each(billettyper, function () {
+            $dropdown.append($("<option />").val(this.billettype).text(this.billettype));
+        });
+    }
 
-            // Endrer style på aktivt element
-            leggTilAktiv(elmt);
+    // Gir brukeren live-stoppforslag
+    function stoppforslag(inputfelt, utskrift, stoppArray, feilmelding) {
+        var fokusert;
 
-        // Hvis brukeren trykker piltast opp
-        } else if (e.keyCode == 38) {
+        // Eventlisner på intput feltet 
+        inputfelt.on("input", function (e) {
+            let stoppListe, stoppElement, i
+            let val = this.value;
 
-            // Flytter pekeren til aktivt element en tilbake i listen 
-            fokusert--;
+            // Lukker eksiterende liste
+            lukkAlleLister();
+            if (!val) { return false; }
+            fokusert = -1;
 
-            // Endrer style på aktivt element
-            leggTilAktiv(elmt);
+            // Lager en div som vil inneholde stoppene
+            stoppListe = document.createElement("div");
+            stoppListe.setAttribute("id", this.id + "stoppListe");
+            stoppListe.setAttribute("class", "stoppListe");
+            this.append(stoppListe);
 
-        // Hvis bruker trykker ENTER på en fokusert stopp i listen
-        } else if (e.keyCode == 13) {
+            // Looper gjennom alle stoppene i listen med stopp
+            for (i = 0; i < stoppArray.length; i++) {
 
-            e.preventDefault();
-            if (fokusert > -1) {
-                if (elmt) {
-                    elmt[fokusert].click();
+                // Sjekker om stopp i listen starter med de samme bokstavene som input
+                if (stoppArray[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+                    feilmelding.html("");
+
+                    // Opretter en div for hvert stopp som marcher
+                    stoppElement = document.createElement("DIV");
+
+                    // Gjør bokstavene som matcher bold
+                    stoppElement.innerHTML = "<strong>" + stoppArray[i].substr(0, val.length) + "</strong>";
+                    stoppElement.innerHTML += stoppArray[i].substr(val.length);
+
+                    // Tar vare på verdien til stoppene som matcher i et input feltet
+                    stoppElement.innerHTML += "<input type='hidden' value='" + stoppArray[i] + "'>";
+
+                    // Event listner for om noen trykker på et av de foreslåtte stoppene
+                    stoppElement.addEventListener("click", function (e) {
+
+                        // Fyller input feltet med stoppet som brukeren trykker på 
+                        inputfelt.val(this.getElementsByTagName("input")[0].value);
+
+                        // Lukker listen med forslag til stopp
+                        lukkAlleLister();
+                    });
+
+                    // Legger stopp elementet til stoppListe elementet og skriver listen ut til brukeren
+                    stoppListe.appendChild(stoppElement);
+                    utskrift.append(stoppListe);
                 }
             }
+        });
+
+        // EventListener på om en tast blir trykket
+        inputfelt.on("keydown", function (e) {
+            var elmt = document.getElementById(this.id + "stoppListe");
+            if (elmt) elmt = elmt.getElementsByTagName("div");
+
+            // Hvis brukeren trykker piltast ned
+            if (e.keyCode == 40) {
+
+                // Flytter pekeren til aktivt element en videre i listen
+                fokusert++;
+
+                // Endrer style på aktivt element
+                leggTilAktiv(elmt);
+
+                // Hvis brukeren trykker piltast opp
+            } else if (e.keyCode == 38) {
+
+                // Flytter pekeren til aktivt element en tilbake i listen 
+                fokusert--;
+
+                // Endrer style på aktivt element
+                leggTilAktiv(elmt);
+
+                // Hvis bruker trykker ENTER på en fokusert stopp i listen
+            } else if (e.keyCode == 13) {
+
+                e.preventDefault();
+                if (fokusert > -1) {
+                    if (elmt) {
+                        elmt[fokusert].click();
+                    }
+                }
+            }
+        });
+
+        // Setter et element til å være aktivt
+        function leggTilAktiv(elmt) {
+            if (!elmt) return false;
+
+            // Fjerner style for aktivt element fra alle elementene
+            fjernAktiv(elmt);
+            if (fokusert >= elmt.length) fokusert = 0;
+            if (fokusert < 0) fokusert = (elmt.length - 1);
+
+            // Legger til style-klasse for aktivt element 
+            elmt[fokusert].classList.add("forslag-active");
         }
-    });
 
-    // Setter et element til å være aktivt
-    function leggTilAktiv(elmt) {
-        if (!elmt) return false;
-
-        // Fjerner style for aktivt element fra alle elementene
-        fjernAktiv(elmt);
-        if (fokusert >= elmt.length) fokusert = 0;
-        if (fokusert < 0) fokusert = (elmt.length - 1);
-
-        // Legger til style-klasse for aktivt element 
-        elmt[fokusert].classList.add("forslag-active");
-    }
-
-    // Fjerner style-klassen for aktivt element fra alle elementer
-    function fjernAktiv(elmt) {
-        for (var i = 0; i < elmt.length; i++) {
-            elmt[i].classList.remove("forslag-active");
+        // Fjerner style-klassen for aktivt element fra alle elementer
+        function fjernAktiv(elmt) {
+            for (var i = 0; i < elmt.length; i++) {
+                elmt[i].classList.remove("forslag-active");
+            }
         }
-    }
 
-    // Lukker listen med forslag til stopp 
-    function lukkAlleLister(elmt) {
-        var elmt = document.getElementsByClassName("stoppListe");
-        for (var i = 0; i < elmt.length; i++) {
-            if (elmt != elmt[i] && elmt != inputfelt) {
-                elmt[i].parentNode.removeChild(elmt[i]);
+        // Lukker listen med forslag til stopp 
+        function lukkAlleLister(elmt) {
+            var elmt = document.getElementsByClassName("stoppListe");
+            for (var i = 0; i < elmt.length; i++) {
+                if (elmt != elmt[i] && elmt != inputfelt) {
+                    elmt[i].parentNode.removeChild(elmt[i]);
+                }
             }
         }
     }

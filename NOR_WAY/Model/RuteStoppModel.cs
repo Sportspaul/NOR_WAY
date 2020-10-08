60c7571ef4027f5840e81ebad3c0c64e70461c64
danielrogerh <daniel.brann@hotmail.no>
2020-10-08 15:07:49 +0200

    RuteStoppModel: Attributter og Regex

--- NOR_WAY/Model/RuteStoppModel.cs
+++ NOR_WAY/Model/RuteStoppModel.cs
@@ -1,6 +1,19 @@
-﻿namespace NOR_WAY.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NOR_WAY.Model
 {
     public class RuteStoppModel
     {
+        [RegularExpression(@"[0-9]{1,2}")]
+        public int StoppNummer { get; set; }
+
+        [RegularExpression(@"[0-9]{1,3}")]
+        public int MinutterTilNesteStopp { get; set; }
+
+        [RegularExpression(@"[a-zA-ZæøåÆØÅ. \-]{2,50}")]
+        public string Stoppnavn { get; set; }
+
+        [RegularExpression(@"(NW)[0-9]{1,4}")]
+        public string Linjekode { get; set; }
     }
 }

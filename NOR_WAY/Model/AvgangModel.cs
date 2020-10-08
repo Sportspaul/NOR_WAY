4a91b8e979d45a21e046fb6aacc92556e3e834a6
danielrogerh <daniel.brann@hotmail.no>
2020-10-08 15:02:36 +0200

    AvgangModel: Attributter og Regex

--- NOR_WAY/Model/AvgangModel.cs
+++ NOR_WAY/Model/AvgangModel.cs
@@ -1,7 +1,17 @@
-﻿namespace NOR_WAY.Model
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace NOR_WAY.Model
 {
     public class AvgangModel
     {
+        [RegularExpression(@"([0-9]{4})[-]([0-9]{2})[-]([0-9]{2})")]
+        public DateTime Avreise { get; set; }
 
+        [RegularExpression(@"[0-9]{1,2}")]
+        public int SolgteBilletter { get; set; } = 0;
+
+        [RegularExpression(@"(NW)[0-9]{1,4}")]
+        public string Linjekode { get; set; }
     }
 }

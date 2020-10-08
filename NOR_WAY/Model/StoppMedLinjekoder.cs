bf59f5cf41eed06cb955730894706d69e101de9b
danielrogerh <daniel.brann@hotmail.no>
2020-10-08 15:04:22 +0200

    StoppMedLinjekode: Regex

--- NOR_WAY/Model/StoppMedLinjekoder.cs
+++ NOR_WAY/Model/StoppMedLinjekoder.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace NOR_WAY.Model
 {
     public class StoppMedLinjekoder
     {
+        [RegularExpression(@"[1-9][0-9]{0,8}")]
         public int Id { get; set; }
+
+        [RegularExpression(@"[a-zA-ZæøåÆØÅ. \-]{2,50}")]
         public string Stoppnavn { get; set; }
+
+        [RegularExpression(@"(NW)[0-9]{1,4}")]
         public List<string> Linjekoder{get; set;}
     }
 } 

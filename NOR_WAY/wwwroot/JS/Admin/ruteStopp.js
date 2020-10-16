ace925165242ea0790e53ace395e1355454c24ac
danielrogerh <daniel.brann@hotmail.no>
2020-10-16 21:39:45 +0200

    Frontend: RuteStopp Delete - Admin kan slette RuteStopp

--- NOR_WAY/wwwroot/JS/Admin/ruteStopp.js
+++ NOR_WAY/wwwroot/JS/Admin/ruteStopp.js
@@ -1,6 +1,19 @@
-let url = window.location.href.split("=");
-linjekode = url[1].split("&")
-linjekode = linjekode[0];
+$(function () {    
+    let url = window.location.href.split("=");
+    linjekode = url[1].split("&")
+    linjekode = linjekode[0];
+    
+    let res = $.get(`../RuteStopp/HentRuteStopp?linjekode=${linjekode}`);
+    lagTabell(res, "CUD", "nyRuteStopp.html?NW431");
+    Hjelpemetoder.endreBakgrunn();
+});
 
-let res = $.get(`../RuteStopp/HentRuteStopp?linjekode=${linjekode}`);
-lagTabell(res, "CUD", "nyRuteStopp.html?NW431");
+function slettRad(id) {
+    $.get(`../RuteStopp/FjernRuteStopp?id=${id}`, function () {
+        $(`#${id}`).remove();
+    })
+    .fail(function () {
+        console.log("Ruten ble IKKE fjernet");
+    });;
+}
+

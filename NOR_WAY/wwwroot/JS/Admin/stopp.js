aa8a5968d8a51c1f98bb8523e7da71b5c90bba50
HermanRustad <hvrustad@gmail.com>
2020-10-16 22:13:32 +0200

    lagt til IDer i alle tabellens celler nesten ferdig filtrering av stopp

--- NOR_WAY/wwwroot/JS/Admin/stopp.js
+++ NOR_WAY/wwwroot/JS/Admin/stopp.js
@@ -1,3 +1,23 @@
 ﻿$(function () {
     Hjelpemetoder.endreBakgrunn();
 });
+
+function filterStoppListe() {
+    let filter = document.querySelector("#filterStopp");
+
+    filter.addEventListener("keyup", (e) => {
+        let brukerInput = e.target.value.toLowerCase();
+        let stoppNavn = document.querySelectorAll("#navn");
+
+
+        stoppNavn.forEach((item) => {
+            if (item.textContent.toLowerCase().indexOf(brukerInput) != -1) {
+                console.log(item);
+                item.closest("tr").style.display = "block";
+            }
+            else {
+                item.closest("tr").style.display = "none";
+            }
+        });
+    });
+}
--- NOR_WAY/wwwroot/JS/TabellMaker.js
+++ NOR_WAY/wwwroot/JS/TabellMaker.js
@@ -1,83 +1,85 @@
 ﻿async function lagTabell(res, CUD, link) {
-        let data = await res;
-        // Primary key fra JSON objektet
-        let id;
-        for (let key in data[0]) {
-            id = key;
-            break;
-        }
-
-        // Sjekker om tabellen skal inneholde en "Create" knapp
-        if (CUD.includes("C")) {
-            const knapp = `
-                <a href="${link}">
-                    <button class="btn btn-success btn-sm">Opprett ny</button>
-                </a>`;
-
-            // Legger knappen til en container
-            const knappContainer = $("#knapp");
-            knappContainer.html(knapp); 
-        }
-
-        // Sjekker om tabellen skal inneholde en "Update" knapp
-        if (CUD.includes("U")) {
-            data.forEach((rad) => {
-                rad.oppdater = `
-                    <a href="${link}?${rad[id]}">
-                        <button class="btn btn-primary btn-sm" value="${rad[id]}">Oppdater</button>
-                    </a>`;
-            });
-          
+    let data = await res;
+    // Primary key fra JSON objektet
+    let id;
+    for (let key in data[0]) {
+        id = key;
+        break;
     }
-        // Sjekker om tabellen skal inneholde en "Delete" knapp
-        if (CUD.includes("D")) {    
-            data.forEach((rad) => {
-                rad.slett = `<button class="btn btn-danger btn-sm" value="${rad[id]}" onclick="slettRad('${rad[id]}')">Slett</button>`;
-            })
-        }
 
-        // Henter nøklene i JSON objektet for overskrift i tabell. Ignorerer nøkler som heter "id"
-        const kolonner = [];
-        
+    // Sjekker om tabellen skal inneholde en "Create" knapp
+    if (CUD.includes("C")) {
+        const knapp = `
+            <a href="${link}">
+                <button class="btn btn-success btn-sm">Opprett ny</button>
+            </a>`;
+
+        // Legger knappen til en container
+        const knappContainer = $("#knapp");
+        knappContainer.html(knapp); 
+    }
+
+    // Sjekker om tabellen skal inneholde en "Update" knapp
+    if (CUD.includes("U")) {
         data.forEach((rad) => {
-            for (let key in rad) {
-                if (kolonner.indexOf(key) === -1) {
-                    if (key != "id") {
-                        kolonner.push(key);
-                    }
+            rad.oppdater = `
+                <a href="${link}?${rad[id]}">
+                    <button class="btn btn-primary btn-sm" value="${rad[id]}">Oppdater</button>
+                </a>`;
+        });
+          
+}
+    // Sjekker om tabellen skal inneholde en "Delete" knapp
+    if (CUD.includes("D")) {    
+        data.forEach((rad) => {
+            rad.slett = `<button class="btn btn-danger btn-sm" value="${rad[id]}" onclick="slettRad('${rad[id]}')">Slett</button>`;
+        })
+    }
+
+    // Henter nøklene i JSON objektet for overskrift i tabell. Ignorerer nøkler som heter "id"
+    const kolonner = [];
+        
+    data.forEach((rad) => {
+        for (let key in rad) {
+            if (kolonner.indexOf(key) === -1) {
+                if (key != "id") {
+                    kolonner.push(key);
                 }
             }
+        }
+    });
+
+    // Lager tabell
+    let tabell = document.createElement("table");
+    tabell.className = "table";
+    tabell.id = "tabell";
+
+    // Lager overskriftene i tabellen
+    let tr = tabell.insertRow(-1);
+    tr.id = "overskrifter";
+
+    kolonner.forEach((overskrift) => {
+        let th = document.createElement("th");
+        th.id = overskrift;
+        th.innerHTML = Hjelpemetoder.fjernCamelCasing(overskrift);
+        tr.appendChild(th);
+    });
+
+    // Legger til all dataen fra JSON objektet i rader
+    data.forEach((rad) => {
+        tr = tabell.insertRow(-1);
+        tr.id = "rad"
+
+        kolonner.forEach((overskrift) => {
+            let celle = tr.insertCell(-1);
+            celle.innerHTML = rad[overskrift];
+            celle.id = overskrift
         });
+    });
 
-        // Lager tabell
-        let tabell = document.createElement("table");
-        tabell.className = "table";
-        tabell.id = "tabell";
-
-        // Lager overskriftene i tabellen
-        let tr = tabell.insertRow(-1);
-        tr.id = "overskrifter";
-
-        for (let i = 0; i < kolonner.length; i++) {
-            let th = document.createElement("th");
-            th.innerHTML = Hjelpemetoder.fjernCamelCasing(kolonner[i]);
-                tr.appendChild(th);
-        }
-
-        // Legger til all dataen fra JSON objektet i rader
-        for (let i = 0; i < data.length; i++) {
-            tr = tabell.insertRow(-1);
-            tr.id = data[i][id];
-
-            for (let j = 0; j < kolonner.length; j++) {
-                let tabCell = tr.insertCell(-1);
-                tabCell.innerHTML = data[i][kolonner[j]];
-            }
-        }
-
-        // Legger tabellen til en container
-        const tabellContainer = document.getElementById("tabellContainer");
-        tabellContainer.appendChild(tabell);
+    // Legger tabellen til en container
+    const tabellContainer = document.getElementById("tabellContainer");
+    tabellContainer.appendChild(tabell);
 }
 
 async function lagRuteoversikt(res, link) {
@@ -106,10 +108,6 @@ async function lagAvgangOversikt(res, CUD, link) {
 
 async function lagStoppoversikt(res) {
     let data = await res;
-    data.forEach((rad) => {
-    //    url = `ruteStopp.html?linjekode=${rad.linjekode}&side=0`
-    //    rad.avganger ;
-    });
     lagTabell(data, "U");
 }
 

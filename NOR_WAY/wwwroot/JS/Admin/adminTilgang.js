2f7d9c30b49e4b280fb86e29b6a3f736459e3de9
Paul Høglend <pmhoglend@gmail.com>
2020-10-19 13:10:55 +0200

    Uten admintilgang kommer man ikke innpå adminsidene

---
+++ NOR_WAY/wwwroot/JS/Admin/adminTilgang.js
@@ -0,0 +1,12 @@
+﻿$(function () {
+    harAdminTilgang();
+});
+
+//Hvis bruker ikke er admin, sendes man til startsiden
+function harAdminTilgang() {
+    $.get("../Brukere/AdminTilgang", function (innlogget) {
+        if (!innlogget) {
+            window.location.href = '../index.html';
+        }
+    });
+}

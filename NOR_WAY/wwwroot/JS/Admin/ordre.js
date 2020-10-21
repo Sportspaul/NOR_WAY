7b59ff417d4288131a85f849a8e60c34080d4033
danielrogerh <daniel.brann@hotmail.no>
2020-10-21 17:07:03 +0200

    Tilbakemelding til bruker hvis ingen ordre funnet

--- NOR_WAY/wwwroot/JS/Admin/ordre.js
+++ NOR_WAY/wwwroot/JS/Admin/ordre.js
@@ -1,18 +1,29 @@
-﻿async function sokEtterOrdre() {
-    const epost = $("#ordreEpost").val();
-    let data = await $.get(`../Ordre/HentOrdre?epost=${epost}`);
+﻿$(() => {
+  Hjelpemetoder.endreBakgrunn();
+});
+
+async function sokEtterOrdre() {
+  const epost = $("#ordreEpost").val();
+  $.get(`../Ordre/HentOrdre?epost=${epost}`, (data) => {
     data.forEach((rad) => {
-        rad.billettyper = Hjelpemetoder.formaterValgteBillettyper(rad.billettyper);
+      rad.billettyper = Hjelpemetoder.formaterValgteBillettyper(
+        rad.billettyper
+      );
     });
     $("#tabellContainer").html("");
     lagTabell(data, "D");
+  }).fail(() => {
+    $("#tabellContainer").html("");
+    $("#feilEpost").html(
+      "Det er ingen ordre knyttet til e-postadressen i søket ditt"
+    );
+  });
 }
 
 function slettRad(id) {
-    $.get(`../Ordre/SlettOrdre?id=${id}`, () => {
-        $(`#${id}`).remove();
-    })
-        .fail(() => {
-            console.log("Ordren ble IKKE fjernet");
-        });;
+  $.get(`../Ordre/SlettOrdre?id=${id}`, () => {
+    $(`#${id}`).remove();
+  }).fail(() => {
+    console.log("Ordren ble IKKE fjernet");
+  });
 }

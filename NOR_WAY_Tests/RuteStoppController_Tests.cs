670a8299d2ed67b4c48b35d03c3b0be7b5634c52
Paul Høglend <pmhoglend@gmail.com>
2020-10-21 16:46:16 +0200

    FjernRuteStopp er enhetstestet

--- NOR_WAY_Tests/RuteStoppController_Tests.cs
+++ NOR_WAY_Tests/RuteStoppController_Tests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
@@ -121,6 +122,73 @@ namespace NOR_WAY_Tests
             //assert
             Assert.Equal("Feil i inputvalideringen på server", resultat.Value);
         }
+
+
+        //Metoder som krever innlogging
+
+        [Fact]
+        public async Task FjernRuteStopp_Riktig()
+        {
+            // arrange
+            int id = 1;
+            mockRepo.Setup(rs => rs.FjernRuteStopp(id)).ReturnsAsync(true);
+            MockSession(_innlogget);
+            //act 
+            var resultat = await ruteStoppController.FjernRuteStopp(id) as OkObjectResult;
+
+            //assert
+            Assert.Equal((int) HttpStatusCode.OK, resultat.StatusCode);
+            Assert.Equal($"RuteStopp med id: {id}, ble slettet", resultat.Value);
+        }
+
+        [Fact]
+        public async Task FjernRuteStopp_IkkeTilgang()
+        {
+            // arrange
+            int id = 1;
+            mockRepo.Setup(rs => rs.FjernRuteStopp(id)).ReturnsAsync(true);
+            MockSession(_ikkeInnlogget);
+            //act 
+            var resultat = await ruteStoppController.FjernRuteStopp(id) as UnauthorizedObjectResult;
+
+            //assert
+            Assert.Equal((int)HttpStatusCode.Unauthorized, resultat.StatusCode);
+            Assert.Equal("Ikke innlogget", resultat.Value);
+        }
+
+        [Fact]
+        public async Task FjernRuteStopp_Feil()
+        {
+            // arrange
+            int id = 1;
+            mockRepo.Setup(rs => rs.FjernRuteStopp(id)).ReturnsAsync(false);
+            MockSession(_innlogget);
+            //act 
+            var resultat = await ruteStoppController.FjernRuteStopp(id) as BadRequestObjectResult;
+
+            //assert
+            Assert.Equal((int)HttpStatusCode.BadRequest, resultat.StatusCode);
+            Assert.Equal($"RuteStopp med id: {id}, kunne ikke slettes", resultat.Value);
+        }
+
+        [Fact]
+        public async Task FjernRuteStopp_Regex()
+        {
+            // arrange
+            int id = 1;
+            mockRepo.Setup(rs => rs.FjernRuteStopp(id)).ReturnsAsync(true);
+            ruteStoppController.ModelState.AddModelError("Linjekode", "Feil i inputvalideringen på server");
+            MockSession(_innlogget);
+
+            //act 
+            var resultat = await ruteStoppController.FjernRuteStopp(id) as BadRequestObjectResult;
+
+            //assert
+            Assert.Equal((int)HttpStatusCode.BadRequest, resultat.StatusCode);
+            Assert.Equal("Feil i inputvalideringen på server", resultat.Value);
+        }
+
+
 
 
 
